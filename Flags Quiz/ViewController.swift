72ab18d7db6425debf8d1e00fa92bae3f86ba98a
KarloFab <fabijanickarlo1@gmail.com>
2022-02-02 22:03:24 +0100

    Added shuffled correct answer

--- Flags Quiz/ViewController.swift
+++ Flags Quiz/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,9 +38,14 @@ class ViewController: UIViewController {
     }
     
     func startGame(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         firstFlagButton.setImage(UIImage(named: countries[0]), for: .normal)
         secondFlagButton.setImage(UIImage(named: countries[1]), for: .normal)
         thirdFlagButton.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
 }
